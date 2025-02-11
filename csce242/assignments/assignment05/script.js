be1a6919abb6150c34554150a9c26ab1231b4a1b
Pauleen Chin <143438451+pauleenchin@users.noreply.github.com>
2025-02-10 20:07:45 -0500

    fixed assignment 05

--- csce242/assignments/assignment05/script.js
+++ csce242/assignments/assignment05/script.js
@@ -6,12 +6,20 @@ document.getElementById("say-hello-section").onclick = () => {
 
 //color picker section
 document.getElementById("color-picker").value = 0o0; // Initial value of slider
-document.getElementById("color-picker").onchange = () => {
+document.getElementById("color-picker").oninput = () => {
     const input = document.getElementById("color-picker").value;
     document.getElementById("star").style.color = input;
 }
 
  //image change section
-document.getElementById("rand-pic").onclick = () => {
-    window.location.reload();
-}
+ const images = ["images/bunny01.jpg", "images/bunny02.jpg", "images/bunny03.jpg"];
+ let currentImg = 0;
+ 
+ document.getElementById("rand-img").onclick = () => {
+     currentImg++;
+     if (currentImg >= images.length) {
+         currentImg = 0; 
+     }
+
+     document.getElementById("rand-img").src = images[currentImg];
+ };
