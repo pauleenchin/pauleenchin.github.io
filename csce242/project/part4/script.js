b83bf9fd972fab6a3cf604bf2d487ba8d06bbbac
Pauleen Chin <143438451+pauleenchin@users.noreply.github.com>
2025-02-17 08:58:47 -0500

    fixed toggle bar issues

--- csce242/project/part4/script.js
+++ csce242/project/part4/script.js
@@ -1,11 +1,9 @@
 // nav toggle
-const navToggle = document.querySelector(".nav-toggle");
-const navItems = document.querySelector(".nav-items");
+document.addEventListener("DOMContentLoaded", function () {
+    const navToggle = document.querySelector(".toggle-nav");
+    const navItems = document.querySelector(".nav-items");
 
-navToggle.addEventListener("click", function () {
-    navItems.classList.toggle("show");
+    navToggle.addEventListener("click", function () {
+        navItems.classList.toggle("show");
+    });
 });
-
-document.getElementById("toggle-nav").onclick = () => {
-    document.getElementById("nav-items").classList.toggle("hide-small");
-};
