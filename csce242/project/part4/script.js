782dc320b633a9e20f762594f68bb9e24313afdc
Pauleen Chin <143438451+pauleenchin@users.noreply.github.com>
2025-02-17 08:24:08 -0500

    bar

--- csce242/project/part4/script.js
+++ csce242/project/part4/script.js
@@ -1,3 +1,11 @@
+// nav toggle
+const navToggle = document.querySelector(".nav-toggle");
+const navItems = document.querySelector(".nav-items");
+
+navToggle.addEventListener("click", function () {
+    navItems.classList.toggle("show");
+});
+
 document.getElementById("toggle-nav").onclick = () => {
     document.getElementById("nav-items").classList.toggle("hide-small");
 };
